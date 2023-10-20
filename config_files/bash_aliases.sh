7cefa0a2490164330f6b66ebb27fceff440c42b5
Carter Watson <cartwatson@gmail.com>
2023-10-20 14:02:31 -0600

    update ll=ls -Al

--- config_files/bash_aliases.sh
+++ config_files/bash_aliases.sh
@@ -5,7 +5,7 @@ alias ell="echo -e '---- ll -al ----';"
 alias cds="clear; ell; cd <School Dir>"
 alias cdm="clear; ell; cd <Personal Projects Dir>"
 alias cdd="clear; cd <Other Working Location>"
-alias ll="ls -al"
+alias ll="ls -Al"
 alias vbr="vim ~/.bash_aliases"
 alias sbr="source ~/.bashrc"
 alias ebr="cat ~/.bash_aliases"
@@ -21,7 +21,7 @@ alias gsll="clear; ell; ll; egs; git status;"
 function gcp() { git commit -m "$@" && git push ;}
 function gpsu() { git push --set-upstream origin $(git branch --show-current); }
 # --override builtins
-function cd() { builtin cd "$@" && ls -al; }
+function cd() { builtin cd "$@" && ls -Al; }
 # --windows
 alias e="explorer ."
 alias c="code ."
