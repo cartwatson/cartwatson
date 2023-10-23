3d674a6e39bd3c23b8940eb62a3bf111efe5beef
cartwatson <cartwatson@gmail.com>
2023-10-23 12:44:00 -0600

    update todo_generator to cover more edge cases

--- tools/todo_generator.py
+++ tools/todo_generator.py
@@ -1,4 +1,5 @@
 import os
+import mimetypes
 
 
 def print_todo_set(set, completed=False):
@@ -16,8 +17,11 @@ def extract_info(file_path):
     for line in lines:
         if "TODO:" in line:
             temp = line.split("TODO: ")
-            if len(temp) > 1: todos.add(temp[1])
-            else: todos.add(line)
+            # TODO: RESEARCH: see if theres a way to find if a line is a comment
+            if len(temp) > 1:
+                todos.add(temp[1])
+            else:
+                todos.add(line)
 
     return todos
 
@@ -31,15 +35,18 @@ def process_files(file_dir, file_todos=None):
         # ignore hidden files and directories
         if item.startswith('.'): continue
 
-        # ignore self
-        if item == "todo_generator.py": continue
-
         item_path = os.path.join(file_dir, item)
+        
+        # get type of file so it doesn't search non-text files
+        mime_type, _ = mimetypes.guess_type(item_path)
+
         # recursive call for subdirectory
         if os.path.isdir(item_path):
             file_todos = process_files(item_path, file_todos)
         # extract info and update the set if it's a file (excluding README.md)
         elif os.path.isfile(item_path) and item != 'README.md':
+            # ignore any files that are not text
+            if mime_type is None or (not mime_type.startswith('text')): continue
             file_todos.update(extract_info(item_path))
 
     return file_todos
@@ -55,7 +62,7 @@ def generate_todo_readme(file_dir="./", readme="./README.md"):
 
     header_section = True
     todo_section = False
-    header, footer, todo_line = "", "", ""
+    header, footer, todo_line = "", "", "\n## TODO\n"
     for line in lines:
         # only do work inside todo section
         if todo_section and "# " in line:
@@ -73,7 +80,7 @@ def generate_todo_readme(file_dir="./", readme="./README.md"):
         if todo_section and line.startswith("- [ ]"):
             readme_todos.add(line.split("- [ ] ")[1])
         if todo_section and line.startswith("- [X]"):
-            readme_todos.add(line.split("- [X] ")[1])
+            readme_todos_done.add(line.split("- [X] ")[1])
 
     # read over files for TODO
     file_todos = process_files(file_dir)
@@ -92,7 +99,7 @@ def generate_todo_readme(file_dir="./", readme="./README.md"):
             print_todo_set(file_todos) + \
             print_todo_set(readme_todos) + \
             print_todo_set(readme_todos_done, completed=True) + \
-            '\n' + footer
+            footer
         )
 
     return
