e37fbeb3af60861b5e1c3a4c9ac156043377fdc8
Carter Watson <cartwatson@gmail.com>
2023-10-24 18:26:55 -0600

    fix formatting

--- config_files/bash_aliases.sh
+++ config_files/bash_aliases.sh
@@ -29,4 +29,4 @@ alias c="code ."
 # --markdown
 # pandoc_md2pdf() { pandoc -V geometry:margin=0.75in --pdf-engine=xelatex -s -o "$1.pdf" "$1.md"; }
 # --custom scripts
-function todo_gen() {python3 ~/.todo_gen.py "$@";}
+function todo_gen() { python3 ~/.todo_gen.py "$@"; }
