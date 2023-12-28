1761ad3779ada2ffc2c8f8448751524bd1fd168d
Carter Watson <cartwatson@gmail.com>
2023-12-28 00:28:45 -0700

    update file name to be consistent with bash

--- tools/setup.sh
+++ tools/setup.sh
@@ -6,7 +6,7 @@ cp ../config_files/vimrc.vim ~/.vimrc
 
 # TODO: create function to take file paths as arguments and create "cd<letter>" aliases for them
 cp ../config_files/bash_aliases.sh ~/.bash_aliases
-cp ~/.bashrc ~/.bashrc-backup # create archive of original bashrc
+cp ~/.bashrc ~/.bashrc_original # create archive of original bashrc
 cp ../config_files/bashrc.sh ~/.bashrc
 source ~/.bashrc
 
